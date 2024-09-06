30587f64dd0a1ba095921da2872fb9688dc5a79c
vinaneosoft <vinaneosoft@gmail.com>
2024-09-06 18:01:29 +0530

    commit

---
+++ src/app/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+import  Header  from '../header/Header';
+/* <React.Fragment> </React.Fragment> */
+function App() {
+  // development : JS
+  return (
+    // template : JSX
+    <>  
+        <h3 className='mainHeading'>NEOSOFT EMPLOYEE MANAGEMENT</h3>
+        <Header></Header>
+    </>
+  );
+}
+
+export default App;
---
+++ src/header/Header.js
@@ -0,0 +1,11 @@
+import "./Header.css"
+
+ function Header(){
+    return(
+        <header className="mainheader">
+            <h4 className="subHeading">EMPLOYEE CRUD</h4>
+        </header>
+    );
+}
+
+export default Header
