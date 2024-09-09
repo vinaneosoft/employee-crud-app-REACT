49f10cffd0bd6e4e45fdd6ec0a99b89493b80e4f
vinaneosoft <vinaneosoft@gmail.com>
2024-09-09 18:32:32 +0530

    commit

--- src/components/OfficeLocation.js
+++ src/components/OfficeLocation.js
@@ -1,11 +1,14 @@
-export function OfficeLocation(props){  /* use any variable name */
+//export function OfficeLocation(props){ /* use any variable to hold props */
+export function OfficeLocation({office}){  /* use key declared at parent side */
     return (
-        <article className="border border-5 p-3 bg-white" style={{width:'300px'}}>
+        <article className="border border-5 p-3 bg-white w-25">
             <header>
-                <h4>{props.office.location}</h4>
+               {/*  <h4>{props.office.location}</h4>  */}
+                <h4>{office.location}</h4> 
             </header>
             <address>
-                {props.office.address}
+                {/*  <p>{props.office.address}</p>  */}
+                <p>{office.address}</p>
             </address>
         </article>
     );
