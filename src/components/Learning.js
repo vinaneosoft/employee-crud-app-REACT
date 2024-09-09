6599a9fe2ae8a9f0dece7139396c460ced114ad4
vinaneosoft <vinaneosoft@gmail.com>
2024-09-09 16:34:25 +0530

    commit

--- src/components/Learning.js
+++ src/components/Learning.js
@@ -2,16 +2,46 @@
 
 export function Learning(){
     let companyName="Neosoft"
-    let joinLocation="Rabale";
+    let joinLocation="rabale";
     return (
         <>
             <h5>{companyName}</h5>  {/* JSX expression*/} 
             <div>
                 {
                  joinLocation.toLowerCase()=='rabale'?  
-                 <address>Rabale address</address> : 
-                 <address>Dadar address</address>
+                 <address> 
+                        Sigma IT Park, <br />
+                     Unit No. 501, TTC Industrial Area, <br />
+                      Rabale, Navi Mumbai,  <br />
+                      Maharashtra 400701 
+                 </address> : 
+                 <address>
+                    The Ruby Tower,  <br />
+                    Senapati Bapat Marg,  <br />
+                    Dadar West, Mumbai,  <br />
+                    Maharashtra 400028
+                 </address>
                 }
+            </div>
+            <hr></hr>
+            <div>
+                {joinLocation.toLowerCase()=='rabale' && 
+                <address> 
+                Sigma IT Park, <br />
+                 Unit No. 501, TTC Industrial Area, <br />
+                Rabale, Navi Mumbai,  <br />
+                Maharashtra 400701 
+                </address> }
+            </div>
+            <hr></hr>
+            <div>
+                {joinLocation.toLowerCase()=='dadar' && 
+                <address>
+                The Ruby Tower,  <br />
+                Senapati Bapat Marg,  <br />
+                Dadar West, Mumbai,  <br />
+                Maharashtra 400028
+                </address> }
             </div>
         </>
     );
