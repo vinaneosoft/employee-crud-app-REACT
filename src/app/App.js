fa597f8f5c234c803b8d8e70841f47e5a8c2ced4
vinaneosoft <vinaneosoft@gmail.com>
2024-09-12 19:00:46 +0530

    commit

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import  Header  from '../header/Header';
 import Footer from '../footer/Footer';
 import { Learning } from '../components/Learning';
@@ -8,13 +8,17 @@ import { NeoEmployees } from '../components/NeoEmployees';
 import { Counter } from '../components/Counter';
 import { EmployeeForm } from '../components/EmployeeForm';
 import { AdminLogin } from '../components/AdminLogin';
+import { appName } from './model/data';
+import { MyContext } from './model/context';
 
-const MyContext=React.createContext();
 
 /* <React.Fragment> </React.Fragment> */
 function App() {
-  let  applicationName="NEOSOFT EMPLOYEE MANAGEMENT"
+  let  [applicationName, setName]=useState(appName)
   // development : JS
+  setTimeout(()=>{
+    setName("NEO EMP MGMT")
+  }, 3000)
   return (
     // template : JSX
     <MyContext.Provider value={applicationName}>  
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,13 @@
+import { useContext } from "react";
+import { MyContext } from "../app/model/context";
 
 
 export function Navbar(){
+  let contextData=useContext(MyContext)
 return(
 <nav className="navbar bg-dark navbar-expand-sm bg-body-tertiary" data-bs-theme="dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <a className="navbar-brand" href="#">{contextData}</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
